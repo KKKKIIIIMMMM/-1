9dd83c5d58e99f1803a2f5952ca09cbd278ad405
김동준 <fgf6267622@gmail.com>
2024-06-10 10:41:53 +0900

    Add files via upload

---
+++ main.swift
@@ -0,0 +1,49 @@
+class AddResult{
+    func calculate() -> Double {
+        return 0
+    }
+}
+func Add (a: Double, b: Double) -> Double {
+    return a + b
+}
+print (Add (a: 4.0, b: 3.0))
+//
+
+class SubstractResult{
+    func calculate() -> Double {
+        return 0
+    }
+}
+func Substract(c: Double, d: Double) -> Double {
+    return c - d
+}
+print (Substract (c: 8.0, d: 4.0))
+//
+
+class MultiplyResult{
+    func calculate() -> Double {
+        return 0
+    }
+}
+func Multiply(e: Double, f: Double) -> Double {
+    return e * f
+}
+print (Multiply (e: 4.0, f: 3.0))
+//
+
+class DivideResult{
+    func Divied() -> Double {
+        return 0
+    }
+}
+func Divide(g: Double, h: Double) -> Double {
+    return g / h
+}
+print (Divide (g:6, h:3))
+//
+
+class remainderResult{
+    func remainder() -> Double {
+        return 0
+    }
+}
